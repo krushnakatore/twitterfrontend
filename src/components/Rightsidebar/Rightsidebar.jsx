601b0d41b1b93882078733440c21ff94d58d953f
Krushna Katore <91548932+krushnakatore@users.noreply.github.com>
2023-05-21 18:13:35 +0530

    signIn and Signup api integration and redux setup with login done

--- src/components/Rightsidebar/Rightsidebar.jsx
+++ src/components/Rightsidebar/Rightsidebar.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { TwitterTimelineEmbed, TwitterVideoEmbed } from "react-twitter-embed";
 
 export const Rightsidebar = () => {
-  return <div>Rightsidebar</div>;
+  return (
+    <div style={{ width: "35vh" }}>
+      <TwitterTimelineEmbed
+        sourceType="profile"
+        screenName="saurabhnemade"
+        options={{ height: 400 }}
+        style={{ width: "30%" }}
+      />
+      <TwitterVideoEmbed
+        id={"560070183650213889"}
+        // options={{ height: 100, width: 100 }}
+      />
+    </div>
+  );
 };
